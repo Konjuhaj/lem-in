94f24889a27be99bf26392d7307da3a08206ad54
Besnik <besnik.konjuhaj@hotmail.com>
2020-05-23 18:07:24 +0300

    Some modification.

--- srcs/lemin.c
+++ srcs/lemin.c
@@ -6,7 +6,7 @@
 /*   By: bkonjuha <bkonjuha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/29 23:23:43 by bkonjuha          #+#    #+#             */
-/*   Updated: 2020/04/12 18:56:56 by bkonjuha         ###   ########.fr       */
+/*   Updated: 2020/04/13 13:06:04 by bkonjuha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,6 @@ int		main(int ac, char **av)
 		file = ft_strsplit(line, '\n');
 		farm.ants = ft_atoi(file[0]);
 		connect_rooms(file, &farm, read_rooms(file, &farm));
-		while (find_paths(&farm.source,&farm.sink, limit, 0) != 3)
-			limit++;
 		while (find_paths(&farm.source,&farm.sink, limit, 0) != 3)
 			limit++;
 		print_values(&farm);
--- srcs/testing.c
+++ srcs/testing.c
@@ -6,19 +6,52 @@
 /*   By: bkonjuha <bkonjuha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/10 16:15:44 by bkonjuha          #+#    #+#             */
-/*   Updated: 2020/04/12 13:31:58 by bkonjuha         ###   ########.fr       */
+/*   Updated: 2020/05/10 21:01:19 by bkonjuha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lemin.h"
+#include <stdio.h>
 
-void	print_values(t_farm *farm)
+void moveLeft(int *nums, int size)
 {
-	int i;
+    int i;
 
-	i = -1;
-	while (farm->rooms[++i] != NULL)
-	{
-		ft_printf("room %s, distance %i, length %d, visited %d \n", farm->rooms[i]->name, farm->rooms[i]->distance, farm->rooms[i]->length, farm->rooms[i]->visited);
-	}
+    i = 0;
+    while (++i < size)
+        nums[i - 1] = nums[i];
+	nums[i] = 0;
+}
+
+int removeDuplicates(int* nums, int numsSize)
+{
+    int i;
+    int j;
+    int ret;
+    int temp;
+
+    i = -1;
+    ret = numsSize;
+    while (++i < numsSize)
+    {
+        temp = nums[i];
+		j = i;
+        while (++j < numsSize)
+        {
+            if (temp == nums[j])
+            {
+                moveLeft(nums + j, numsSize - j);
+                ret--;
+            }
+			printf("%d\n", j);
+        }
+    }
+    return (ret);
+}
+
+int main(void)
+{
+	int nums[] = {1, 1, 2};
+	printf("%d",removeDuplicates(nums, 3));
+	return (0);
 }
